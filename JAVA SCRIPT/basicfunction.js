52d521fc399f47931efc112c7041ca29aef5c066
RominMadaliya <rominskillqode@gmail.com>
2024-07-12 12:00:42 +0530

    11-07

---
+++ JAVA SCRIPT/basicfunction.js
@@ -0,0 +1,23 @@
+{
+    /* Convert temperature from Celsius to Fahrenheit */
+    
+    // function temp(celsius){
+    
+    // let fahrenheit = (celsius * 9/5) + 32;
+    // return fahrenheit
+    // }
+    // let celsius = parseInt(prompt("Enter the value of Celsius"));
+    // let fahrenheit = temp(celsius);
+    // console.log(`Value of celsius is :- ${celsius}`);
+    // console.log(`Conversition of celsius into fahrenheit is :- ${fahrenheit}`);
+
+    // }
+
+}
+
+
+{
+
+
+    
+}
